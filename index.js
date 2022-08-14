a5143b06f222092312c7b088e9c9a3e6d92a6173
AakankshaJangid <aakankshajangid929@gmail.com>
2022-08-14 18:45:29 +0530

    Second commit

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const Num = document.querySelector('#numb').value//const Num = document.getElementById('#numb').value
+const inputEl = document.getElementById("input-num")
+const BtnEl = document.getElementById("convert-btn")
 let P1 = document.getElementById("p1")
 let P2 = document.getElementById("p2")
 let P3 = document.getElementById("p3")
-function convertNum(){
-    let length_m_to_f = Num * Number(3.28084)
-    let lengthStr = `${Num} meters = ${length_m_to_f} feets`
-    P1.innerHTML = lengthStr
-}
+BtnEl.addEventListener("click",function(){
+    let mToFeet = inputEl.value * 3.28084
+    let feetToM = inputEl.value * 0.3048
+    P1.innerHTML = `${inputEl.value} meters = ${mToFeet.toFixed(3)} feets | ${inputEl.value} feets = ${feetToM.toFixed(3)} meters`
+    let lToGallon = inputEl.value * 0.264172
+    let gToLiter = inputEl.value * 3.78541103373138
+    P2.innerHTML = `${inputEl.value} liters = ${lToGallon.toFixed(3)} gallons | ${inputEl.value} gallons = ${gToLiter.toFixed(3)} liters`
+    let kToPounds = inputEl.value * 2.20462
+    let poundsToK = inputEl.value * 0.453592
+    P3.innerHTML = `${inputEl.value} kilos = ${kToPounds.toFixed(3)} pounds | ${inputEl.value} pounds = ${poundsToK.toFixed(3)} kilos`
+})
